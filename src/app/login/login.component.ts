32240c76bccbe5ed4de6fc550e4f11c7f8700fd3
thiago-santanna <uzmaki_thiago@outlook.com>
2024-03-25 15:56:54 -0300

    tela de perfil criada em ver. 0.1

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,10 +1,19 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent {
+  constructor(private router: Router) {}
 
+  goToPerfil() {
+    this.router.navigate(['/perfil']);
+  }
+
+  goToDashboard() {
+    this.router.navigate(['/dashboard']);
+  }
 }
--- src/app/modules/perfil/perfil.module.ts
+++ src/app/modules/perfil/perfil.module.ts
@@ -3,8 +3,16 @@ import { CommonModule } from '@angular/common';
 import { PerfilComponent } from './pages/perfil/perfil.component';
 import { PerfilModuleRoutingModule } from './perfil-module-routing.module';
 
+import { ButtonModule } from 'primeng/button';
+import { DividerModule } from 'primeng/divider';
+
 @NgModule({
   declarations: [PerfilComponent],
-  imports: [CommonModule, PerfilModuleRoutingModule],
+  imports: [
+    CommonModule,
+    PerfilModuleRoutingModule,
+    ButtonModule,
+    DividerModule,
+  ],
 })
 export class PerfilModule {}
