2a400fe7b6776db35cfe60bf53bf59f1415d7921
thiago-santanna <uzmaki_thiago@outlook.com>
2024-04-13 09:13:31 -0300

    Header da dachboard

--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -3,8 +3,16 @@ import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DashboardModuleRoutingModule } from './dashboard-module-routing.module';
 
+import { ImageModule } from 'primeng/image';
+import { ButtonModule } from 'primeng/button';
+
 @NgModule({
   declarations: [DashboardComponent],
-  imports: [CommonModule, DashboardModuleRoutingModule],
+  imports: [
+    CommonModule,
+    DashboardModuleRoutingModule,
+    ImageModule,
+    ButtonModule,
+  ],
 })
 export class DashboardModule {}
