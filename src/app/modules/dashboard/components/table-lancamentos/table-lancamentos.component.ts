96195137ed3edcd179f5adf273e795279bb2b309
thiago-santanna <uzmaki_thiago@outlook.com>
2024-04-13 11:21:52 -0300

    Ajustes e card financeiro

---
+++ src/app/modules/dashboard/components/table-lancamentos/table-lancamentos.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-table-lancamentos',
+  templateUrl: './table-lancamentos.component.html',
+  styleUrls: ['./table-lancamentos.component.scss']
+})
+export class TableLancamentosComponent {
+
+}
--- src/app/modules/dashboard/dashboard.module.ts
+++ src/app/modules/dashboard/dashboard.module.ts
@@ -2,18 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DashboardModuleRoutingModule } from './dashboard-module-routing.module';
+import { CardFinanceiroComponent } from './components/card-financeiro/card-financeiro.component';
 
 import { ImageModule } from 'primeng/image';
 import { ButtonModule } from 'primeng/button';
-import { CardFinanceiroComponent } from './components/card-financeiro/card-financeiro.component';
+import { TableLancamentosComponent } from './components/table-lancamentos/table-lancamentos.component';
 
 @NgModule({
-  declarations: [DashboardComponent, CardFinanceiroComponent],
+  declarations: [DashboardComponent, CardFinanceiroComponent, TableLancamentosComponent],
   imports: [
-    CommonModule,
-    DashboardModuleRoutingModule,
     ImageModule,
+    CommonModule,
     ButtonModule,
+    DashboardModuleRoutingModule,
   ],
 })
 export class DashboardModule {}
